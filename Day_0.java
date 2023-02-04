a71e080b66766f27d96a419596177d09e930c38b
studuhubtutorial <harendraprajapati72@gmail.com>
2023-02-05 01:41:31 +0530

    Day_0_challange

---
+++ Day_0.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Day_0 {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.print(str); 
+    }
+}
